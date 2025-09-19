6afb21971f8953724e693720fe105202d0ffee50
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2025-09-19 07:00:20 -0500

    ultimos

--- src/application/use-cases/tiquetera/UpdateTiquetera.js
+++ src/application/use-cases/tiquetera/UpdateTiquetera.js
@@ -14,24 +14,13 @@ export default class UpdateTiquetera {
         }
 
         const totalTransaccionesActualizadas = tiquetera.totalTransacciones + 1;
-<<<<<<< HEAD
-
-        const datosParaActualizar = {
-            ...datosActualizados,
-            totalTransacciones: totalTransaccionesActualizadas
-        };
-=======
->>>>>>> 6a249dc3d2f5189f5ab624270d943c9ef210a0d3
 
         const datosParaActualizar = {
             ...datosActualizados,
             totalTransacciones: totalTransaccionesActualizadas
         };
 
-<<<<<<< HEAD
-=======
-      
->>>>>>> 6a249dc3d2f5189f5ab624270d943c9ef210a0d3
+
         const tiqueteraActualizada = await this.tiqueteraRepository.update(id, datosParaActualizar);
 
         return tiqueteraActualizada;
